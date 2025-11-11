7165fdc4c55283830e1572d70ba2696100731bd0
dkolonay <daniel.kolonay@gmail.com>
2025-11-10 19:24:00 -0500

    backend actually connected...

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/test-get', function(req, res, next) {
+router.get('/api/test-get', function(req, res, next) {
   res.json({ title: 'Express' });
 });
 
