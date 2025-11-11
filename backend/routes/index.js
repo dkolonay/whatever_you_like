96c66112793e1abc6fb6fd6c9b07c66955580a23
dkolonay <daniel.kolonay@gmail.com>
2025-11-10 21:05:50 -0500

    database files updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+import { Router } from 'express';
+
+var router = Router();
 
 /* GET home page. */
 router.get('/api/test-get', function(req, res, next) {
   res.json({ title: 'Express' });
 });
 
-module.exports = router;
+export default router;
